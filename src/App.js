913f63f92bacd5ff16fd452df3e04cae91185f8f
Malek AITSISAID <aitsisaidmalek@gmail.com>
2024-02-02 12:41:48 +0100

    react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Fragment } from 'react';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+import Button from 'react-bootstrap/Button';
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className='container'>
+    <Navbar expand="lg" className="bg-body-tertiary">
+     <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+     <Navbar.Collapse id="basic-navbar-nav">
+       <Nav className="me-auto">
+         <Nav.Link href="#home">Home</Nav.Link>
+         <Nav.Link href="#link">Link</Nav.Link>
+         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+           <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+           <NavDropdown.Item href="#action/3.2">
+             Another action
+           </NavDropdown.Item>
+           <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+           <NavDropdown.Divider />
+           <NavDropdown.Item href="#action/3.4">
+             Separated link
+           </NavDropdown.Item>
+         </NavDropdown>
+       </Nav>
+     </Navbar.Collapse>
+  
+ </Navbar>
+
+
+ <div className='contenue'>
+ <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://img.freepik.com/free-vector/bird-colorful-gradient-design-vector_343694-2506.jpg?size=338&ext=jpg&ga=GA1.1.1448711260.1706745600&semt=ais" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+      </ListGroup>
+      <Card.Body>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+
+    <Card style={{ width: '18rem' }}>
+      <Card.Header>Featured</Card.Header>
+      <ListGroup variant="flush">
+        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+      </ListGroup>
+    </Card>
+
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+ </div>
+
+  
+   </div>
   );
 }
 
